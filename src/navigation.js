18bb1a5d146c6a981f9aab984ccbf723d173e933
Benedict Schmieder <benedict.schmieder+github@glueckkanja-gab.com>
2024-03-19 16:47:04 +0100

    Add locally developed changes

--- src/navigation.js
+++ src/navigation.js
@@ -1,5 +1,70 @@
-import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+import { getPermalink, getBlogPermalink } from './utils/permalinks';
 
+export const headerData = {
+  links: [
+    {
+      text: 'Home',
+      href: getPermalink('/')
+    },
+    {
+      text: 'Dienstleistungen',
+      href: getPermalink('/services')
+    },
+    {
+      text: 'Preise',
+      href: getPermalink('/pricing')
+    },
+    {
+      text: 'Über mich',
+      href: getPermalink('/about')
+    },
+    {
+      text: 'Blog',
+      href: getBlogPermalink()
+    }
+  ],
+  actions: [{ text: 'Kontakt', href: getPermalink('/contact'), target: '_blank' }]
+}
+
+export const footerData = {
+  links: [
+    {
+      title: 'Benedict Schmieder',
+      links: [
+        { text: 'Über mich', href: getPermalink('/services') },
+        { text: 'Blog', href: getBlogPermalink() },
+        { text: 'Kontakt', href: getPermalink('/contact') }
+      ],
+    },
+    {
+      title: 'IT-Services',
+      links: [
+        { text: 'Dienstleistungen', href: getPermalink('/services') },
+        { text: 'Preise', href: getPermalink('/pricing') },
+        { text: 'Kontakt', href: getPermalink('/contact') }
+      ],
+    },
+    {
+      title: 'Kleingedrucktes',
+      links: [
+        { text: 'Impressum', href: getPermalink('/legal-notice') },
+        { text: 'Datenschutzerklärung', href: getPermalink('/privacy') },
+        { text: 'Kontakt', href: getPermalink('/contact') }
+      ]
+    },
+  ],
+  secondary: [
+    { text: 'Für eine möglichst benutzerfreundliche Erfahrung verzichte ich auf Cookies. Es werden keine Aktivitäten gespeichert weshalb auch keine Cookie-Hinweise mehr bestätigt werden müssen.' }
+  ],
+  socialLinks: [
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://twitter.com/B_Schmieder' },
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/benedict-schmieder/' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/benedictschmieder' },
+  ],
+  footNote: 'Copyright © 2024 Benedict Schmieder IT-Solutions',
+};
+
+/*
 export const headerData = {
   links: [
     {
@@ -181,3 +246,4 @@ export const footerData = {
     Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · All rights reserved.
   `,
 };
+*/
